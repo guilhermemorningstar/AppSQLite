401f68625eebeafbe84b34862c383e940aa66764
guilhermemorningstar <guilhermemorningstar@yahoo.com>
2020-07-09 19:00:29 -0300

    Finalizado

---
+++ app/src/main/java/com/example/appsqlite/DatabaseHelper.java
@@ -0,0 +1,74 @@
+package com.example.appsqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "DBLogin.sql", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table tbUsuario(cpf text primary key, nome text, rg text, telefone text, email text, senha text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists tbUsuario");
+    }
+    //inserindo valores no banco de dados
+
+    public boolean insert(String nome, String cpf, String rg, String telefone, String email, String senha) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        //ContentValues é quem pega as informações da tabela interna para colocar no Banco de Dados
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("nome", nome);
+        contentValues.put("cpf", cpf);
+        contentValues.put("rg", rg);
+        contentValues.put("telefone", telefone);
+        contentValues.put("email", email);
+        contentValues.put("senha", senha);
+
+        long inserido = db.insert("tbUsuario", null, contentValues);
+        //nullColumnHack é o parâmetro que define o que será adicionado, caso nada seja colocado
+
+        if (inserido == 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Boolean validarCPF(String cpf) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        //Para converter algum tipo diferente de String basta utilizar convert.toString("nome_da_variável")
+        Cursor cursor = db.rawQuery("select * from tbUsuario where cpf = ?", new String[]{cpf});
+        if (cursor.getCount() > 0) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    //Verificando usuário e senha
+
+    public Boolean checarCPFSenha(String cpf, String senha) {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from tbUsuario where cpf = ? and senha = ?", new String[]{cpf, senha});
+        if (cursor.getCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
